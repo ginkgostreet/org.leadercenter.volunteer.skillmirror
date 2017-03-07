c39c0e6747f201cb8c627995cf2f5c605c73e0ef
Frank J. Gómez <frank@ginkgostreet.com>
2017-03-07 09:16:37 -0500

    VARL-237: Added mirroring logic, with a stub for transformations.

--- api/v3/Skill/Mirror.php
+++ api/v3/Skill/Mirror.php
@@ -32,15 +32,55 @@ function _civicrm_api3_skill_Mirror_spec(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_skill_Mirror($params) {
-  $field = CRM_Utils_Array::value('field', $params);
-  $source = CRM_Utils_Array::value('source', $params, 'Volunteer_Skills');
-  $target = CRM_Utils_Array::value('target', $params, 'Project_Skills');
+  $toMirror = CRM_Utils_Array::value('field', $params);
+  $sourceGroup = CRM_Utils_Array::value('source', $params, 'Volunteer_Skills');
+  $targetGroup = CRM_Utils_Array::value('target', $params, 'Project_Skills');
+  // api.CustomField.create doesn't seem to accept custom group name, so get the ID
+  $context['target_group_id'] = civicrm_api3('CustomGroup', 'getvalue', array(
+    'extends' => 'Individual',
+    'name' => $targetGroup,
+    'return' => 'id',
+  ));
 
-  if ($field == 'all') {
+  if ($toMirror === 'all') {
     $result = array();
+
+    $sourceFields = civicrm_api3('CustomField', 'get', array(
+      'custom_group_id' => $sourceGroup,
+      'options' => array('limit' => 0),
+    ));
+    foreach ($sourceFields['values'] as $sourceField) {
+      // TODO: to make this more extensible, the transform function could be
+      // registered via hook rather than hardcoded
+      $mirroredField = _skill_mirror_transform($sourceField, $context);
+      $exists = civicrm_api3('CustomField', 'getcount', array(
+        'custom_group_id' => $mirroredField['custom_group_id'],
+        'name' => $mirroredField['name'],
+      ));
+      if (!$exists) {
+        $create = civicrm_api3('CustomField', 'create', $mirroredField);
+        $result[$create['id']] = $create['values'];
+      }
+    }
+
     return civicrm_api3_create_success($result, $params, 'Skill', 'Mirror');
   } else {
     throw new API_Exception("Support for mirroring individual fields hasn't been
         implemented yet");
   }
+}
+
+/**
+ * Transforms parameters from the fetched (to be mirrored) field for use in
+ * creating the new field.
+ *
+ * @param array $field
+ *   The original field, to be mirrored.
+ * @param array $context
+ *   Context from the calling function which may aid in transformation.
+ * @return array
+ *   An API-ready set of customField parameters.
+ */
+function _skill_mirror_transform(array $field, array $context) {
+
 }
