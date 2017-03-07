dba2495d0209c9f25dcd72c3c4e4da68824c8201
Frank J. Gómez <frank@ginkgostreet.com>
2017-03-07 08:07:49 -0500

    VARL-237: Added target custom group to collect mirrored skills.

---
+++ managed/Mirror.mgd.php
@@ -0,0 +1,20 @@
+<?php
+// This file declares a managed database record of type "Custom Group".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// https://docs.civicrm.org/dev/en/master/hooks/hook_civicrm_managed/
+return array (
+  0 =>
+  array (
+    'name' => 'org.leadercenter.volunteer.skillmirror custom group',
+    'entity' => 'CustomGroup',
+    'params' =>
+    array(
+      'extends' => 'Volunteer Project',
+      'is_reserved' => 1,
+      'name' => 'Project_Skills',
+      'title' => ts('Skills Needed', array('domain' => 'org.leadercenter.volunteer.skillmirror')),
+      'version' => 3,
+    ),
+  ),
+);
