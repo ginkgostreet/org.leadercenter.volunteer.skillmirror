13bcac8cfe2b447b7c23018e3c54e53f975cafa3
Frank J. Gómez <frank@ginkgostreet.com>
2017-03-07 07:58:00 -0500

    VARL-237: Replaced boilerplate with descriptions and general outline of the new API/job.

--- api/v3/Skill/Mirror.mgd.php
+++ api/v3/Skill/Mirror.mgd.php
@@ -2,7 +2,7 @@
 // This file declares a managed database record of type "Job".
 // The record will be automatically inserted, updated, or deleted from the
 // database as appropriate. For more details, see "hook_civicrm_managed" at:
-// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+// https://docs.civicrm.org/dev/en/master/hooks/hook_civicrm_managed/
 return array (
   0 => 
   array (
@@ -11,8 +11,12 @@ return array (
     'params' => 
     array (
       'version' => 3,
-      'name' => 'Call Skill.Mirror API',
-      'description' => 'Call Skill.Mirror API',
+      'name' => 'Skill.Mirror',
+      'description' => 'Creates a custom field on the Volunteer Project entity
+        for each skill-related custom field on the Volunteer contact type. This
+        job can be run safely (i.e., without creating duplicate skills) multiple
+        times, but only need to be run when there are skills on the contact that
+        are not reflected on the project.',
       'run_frequency' => 'Always',
       'api_entity' => 'Skill',
       'api_action' => 'Mirror',
--- api/v3/Skill/Mirror.php
+++ api/v3/Skill/Mirror.php
@@ -1,40 +1,46 @@
 <?php
 
 /**
- * Skill.Mirror API specification (optional)
+ * Skill.Mirror API specification
  * This is used for documentation and validation.
+ *
+ * TODO: This would be more extensible if the source and target custom groups
+ * were parameterized rather than assumed.
  *
  * @param array $spec description of fields supported by this API call
  * @return void
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
  */
 function _civicrm_api3_skill_Mirror_spec(&$spec) {
-  $spec['magicword']['api.required'] = 1;
+  $spec['field'] = array(
+    'api.default' => 'all',
+    'title' => 'Field Name',
+    'description' => 'Which field is being mirrored? "All" will cause all the
+      fields in the source custom group to be evaluated.',
+    'type' => CRM_Utils_Type::T_STRING,
+  );
 }
 
 /**
  * Skill.Mirror API
  *
  * @param array $params
- * @return array API result descriptor
+ * @return array
+ *   The custom field meta data for each newly created field.
  * @see civicrm_api3_create_success
  * @see civicrm_api3_create_error
  * @throws API_Exception
  */
 function civicrm_api3_skill_Mirror($params) {
-  if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
-    $returnValues = array( // OK, return several data rows
-      12 => array('id' => 12, 'name' => 'Twelve'),
-      34 => array('id' => 34, 'name' => 'Thirty four'),
-      56 => array('id' => 56, 'name' => 'Fifty six'),
-    );
-    // ALTERNATIVE: $returnValues = array(); // OK, success
-    // ALTERNATIVE: $returnValues = array("Some value"); // OK, return a single value
+  $field = CRM_Utils_Array::value('field', $params);
+  $source = CRM_Utils_Array::value('source', $params, 'Volunteer_Skills');
+  $target = CRM_Utils_Array::value('target', $params, 'Project_Skills');
 
-    // Spec: civicrm_api3_create_success($values = 1, $params = array(), $entity = NULL, $action = NULL)
-    return civicrm_api3_create_success($returnValues, $params, 'NewEntity', 'NewAction');
+  if ($field == 'all') {
+    $result = array();
+    return civicrm_api3_create_success($result, $params, 'Skill', 'Mirror');
   } else {
-    throw new API_Exception(/*errorMessage*/ 'Everyone knows that the magicword is "sesame"', /*errorCode*/ 1234);
+    throw new API_Exception("Support for mirroring individual fields hasn't been
+        implemented yet");
   }
 }
-
